89720d7fb2ab070133e09f4652fb23b51dcca563
Farid <farid.idbourhim@hotmail.com>
2016-12-25 21:56:03 +0100

    Creation des couches au niveau du WCF, Creation des connections(reste les connections string),

---
+++ WCF/BiblioService/DataAccessLayer/DataBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DataBase
+    {
+        private static SqlConnection _Db = null;
+
+        public static SqlConnection DbAdminConnection
+        {
+            get
+            {
+                if (_Db ==null || _Db.State != ConnectionState.Open)
+                {
+                    _Db = new SqlConnection();
+                    _Db.ConnectionString = Globals.AdminConnectionString;
+                }
+                return _Db;
+            }
+
+            set { }
+        }
+        public static SqlConnection DbLecteurConnection
+        {
+            get
+            {
+                if (_Db == null || _Db.State != ConnectionState.Open)
+                {
+                    _Db = new SqlConnection();
+                    _Db.ConnectionString = Globals.LecteurConnectionString;
+                }
+                return _Db;
+            }
+
+            set { }
+        }
+        public static SqlConnection DbSysAdminConnection
+        {
+            get
+            {
+                if (_Db == null || _Db.State != ConnectionState.Open)
+                {
+                    _Db = new SqlConnection();
+                    _Db.ConnectionString = Globals.SysAdminConnectionString;
+                }
+                return _Db;
+            }
+
+            set { }
+        }
+
+    }
+}
---
+++ WCF/BiblioService/DataAccessLayer/Globals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    class Globals
+    {
+        public static string AdminConnectionString = "";
+        public static string LecteurConnectionString = "";
+        public static string SysAdminConnectionString = "";
+    }
+}
