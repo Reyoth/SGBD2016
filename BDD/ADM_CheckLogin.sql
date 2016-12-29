94388d1d2d2ddda1edb317b402ea72a9f41fd0e8
Reyoth <serialkiwi@gmail.com>
2016-12-29 13:16:52 +0100

    Procedure login to function

--- BDD/ADM_CheckLogin.sql
+++ BDD/ADM_CheckLogin.sql
@@ -1,7 +1,8 @@
+
 USE [SGBD2016_Bibliotheque]
 GO
 
-/****** Object:  StoredProcedure [dbo].[ADM_CheckLogin]    Script Date: 28-12-16 11:48:59 ******/
+/****** Object:  UserDefinedFunction [dbo].[ADM_CheckLogin]    Script Date: 29-Dec-16 13:12:52 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,19 +12,25 @@ GO
 -- =============================================
 -- Author:		<Farid IDBOURHIM, Simon ENCEV>
 -- Create date: <28/12/2016>
--- Description:	<PROCEDURE QUI VERIFIE L''EXISTANCE D''UN ADMINISTRATEUR ET SON PASSWORD POUR LUI PERMETTRE L''ACCESS AU GUI LECTEUR>
+-- Description:	<FUNCTION QUI VERIFIE L''EXISTANCE D''UN ADMINISTRATEUR ET SON PASSWORD POUR LUI PERMETTRE L''ACCESS AU GUI LECTEUR>
 -- =============================================
-CREATE PROCEDURE [dbo].[ADM_CheckLogin]
-	-- Parametres
-	@UserName nvarchar(50),
-	@Password nvarchar(50)
+
+CREATE FUNCTION [dbo].[ADM_CheckLogin] (@UserName nvarchar(50),
+	@Password nvarchar(50))
+RETURNS bit
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT OFF;
+     declare @exist bit
 
-    SELECT  ADM_UserName, ADM_Password FROM Administrateur WHERE ADM_UserName = @UserName AND ADM_Password = @Password
+	 IF exists( SELECT  ADM_UserName, ADM_Password FROM Administrateur WHERE ADM_UserName = @UserName AND ADM_Password = @Password)
+	 SET @exist = 1
+
+	 ELSE
+	 SET @exist = 0
+
+	 RETURN @exist
 END
 
 GO
+
+
