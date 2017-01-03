74943922b6a1a8df906d92cd63b2c24f1b66f750
Farid <farid.idbourhim@hotmail.com>
2017-01-03 17:39:48 +0100

    Entities et error

---
+++ ProjetLocal/Bibliotheque_SGBD2016/ENTITIES/Emprunt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Emprunt
+    {
+        public int Id { get; set; }
+        public DateTime DateEmprunt { get; set; }
+        public DateTime DateRetour { get; set; }
+        public decimal MontantPaye { get; set; }
+        public int ExeId { get; set; }
+        public int LecId { get; set; }
+    }
+}
---
+++ ProjetLocal/Bibliotheque_SGBD2016/ENTITIES/EmpruntEnCours.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class EmpruntEnCours
+    {
+        public int EMP_Id { get; set; }
+        public string EXE_Code { get; set; }
+        public string LIV_Titre { get; set; }
+        public string BIB_Libelle { get; set; }
+        public string LEC_Nom { get; set; }
+        public string LEC_Prenom { get; set; }
+        public System.DateTime EMP_DateEmprunt { get; set; }
+        public Nullable<decimal> EMP_MontantPaye { get; set; }
+    }
+}
