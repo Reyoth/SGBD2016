b5ed4cce39914fd5907d56b745cd44967664994f
Reyoth <serialkiwi@gmail.com>
2017-01-11 00:22:51 +0100

    services admi lecteur

--- ProjetLocal/Bibliotheque_SGBD_2016/ServiceBilbio/IserviceLecteur.cs
+++ ProjetLocal/Bibliotheque_SGBD_2016/ServiceBilbio/IserviceLecteur.cs
@@ -11,56 +11,9 @@ namespace ServiceBilbio
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IServiceLecteur
     {
-        [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        //ADMINISTRATEUR
-        [OperationContract]
-        void EXE_CreerExemplaire(string code, DateTime DateAchat, string BibLibelle, string LivTitre);
-        [OperationContract]
-        void AllExemplairesAllBib(ref DataSet ds);
-        [OperationContract]
-        string ADM_Lgin(string userName, string password);
-        [OperationContract]
-        void AllRetardataires(ref DataSet ds);
-        [OperationContract]
-        void AllLivres(ref DataSet ds);
-        [OperationContract]
-        void BIB_AllLibelle(ref List<string> bibliotheques);
-        [OperationContract]
-        void RetournerExemplaire(int exemplaireId, double montant);
-        [OperationContract]
-        void AllLecteurs(ref DataSet ds);
-        [OperationContract]
-        void AllEmpruntsEnCours(ref DataSet ds);
-        [OperationContract]
-        void LivreByISBN(ref DataSet ds, string isbn);
-        [OperationContract]
-        void LivreByTitre(ref DataSet ds, string titre);
-        [OperationContract]
-        void AjoutManuelLivre(string ISBN, string Titre, string Image, string Auteurs);
-        [OperationContract]
-        void UpdateLivre(string ISBN, string Titre, string Image, string Auteurs);
-        [OperationContract]
-        void DeleteLivre(string ISBN);
-        [OperationContract]
-        void NbEmpruntsByLecId(ref int nbreEmprunts, int LecId);
-        [OperationContract]
-        void NbRetardByLecId(ref int nbRetards, int LecId);
-        [OperationContract]
-        void AllExemplairesByTitle(ref DataSet ds, string Title);
-        [OperationContract]
-        void AllExemplairesByExeCode(ref DataSet ds, string ExeCode);
-        [OperationContract]
-        void EXE_AllExemplaireByBIB_Libelle(ref DataTable dt, string Libelle);
-        [OperationContract]
-        void ChargerLivreTitres(ref List<string> titres);
-
+        
         //LECTEUR
         [OperationContract]
         void BIB_AllLibelle(ref List<BIB_AllLibelle_Result> bibs);
@@ -106,24 +59,5 @@ namespace ServiceBilbio
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceBilbio.ContractType".
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
+    
 }
